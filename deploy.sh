4381844ed9e95eba7c2f0b866cac995e77742452
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2019-11-05 22:41:52 +0100

    Integrate notification service in Docker (#20)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$1" -eq "--demo" ]]; then
+if [[ $1 == "--demo" ]]; then
   docker-compose run --rm start_dependencies_kafka_database
   docker-compose run --rm start_dependencies_spring
   docker-compose up demo
