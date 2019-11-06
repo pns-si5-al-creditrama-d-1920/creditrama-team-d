379038c0a519d2a317a831370ea913540bda2f7c
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2019-11-06 11:13:21 +0100

    Deploy for two environments in France and Greenland

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,12 @@
 if [[ $1 == "--demo" ]]; then
   docker-compose run --rm start_dependencies_kafka_database
   docker-compose run --rm start_dependencies_spring
-  docker-compose up demo
+  #docker-compose up demo
+elif [[ $1 == "--greenland" ]]; then
+  docker-compose -f greenland.yml run --rm start_dependencies_kafka_database
+  docker-compose -f greenland.yml up -d bank
+elif [[ $1 == "--france" ]]; then
+  docker-compose -f france.yml up -d
 else
   docker-compose run --rm start_dependencies_kafka_database
   docker-compose run --rm start_dependencies_spring
